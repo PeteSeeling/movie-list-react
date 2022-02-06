a97baef0b49ee6228decbe240f95592c89500526
Pete Seeling <peteseeling@gmail.com>
2022-02-06 11:59:00 -0600

    working on app js. setting up movie form div

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import MovieItem from './MovieItem';
 import MoviesList from './MovieList';
+import { MovieForm } from './MovieForm';
 
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [filteredMovies, setFilteredMovies] = useState([]);
+
+
+
+  function addMovie(newestMovie) {
+    const updatedMoviesList = [...movies, newestMovie];
+
+    setMovies(updatedMoviesList);
+  }
+
+  function deleteMovie(title) {
+    const index = movies.findIndex(movie => movie.title === title);
+
+    movies.splice(index, 1);
+    setFilter('');
+    setMovies([...movies]);
+
+  }
+
+
+
+
   return (
     <div className="App">
+      <div className='current-movies-list'>
+        <MovieForm
+        movieFormDirector={movieFormDirector}
+        setmovieFormDirector={setmovieFormDirector}
+        movieFormYear={movieFormYear}
+        setmovieFormYear={}
+        movieFormTitle={}
+        setmovieFormTitle={}
+        movieFormColor={}
+        setmovieFormColor={}
+        submitMovie={}
+        />
+      </div>
 
  
     </div>
